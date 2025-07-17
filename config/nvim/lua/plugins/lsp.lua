50f9f41d477bedb32b2c0ad2e46c4ca5d281025e
Bananums <sjohnsen.valle@gmail.com>
2025-07-17 09:53:41 +0200

    made lader not timeout

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -19,7 +19,6 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-
       local lspconfig = require("lspconfig")
       lspconfig.ts_ls.setup({capabilities})
       lspconfig.clangd.setup({})
--- config/nvim/lua/suffering/init.lua
+++
@@ -1,3 +0,0 @@
-require("suffering.remap")
-print("Suffering");
-
